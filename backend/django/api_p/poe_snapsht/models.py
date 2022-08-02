6c5005c7055328fde1f8b320c92ea3a1e00d22ab
fluxink <pavelvolk300@gmail.com>
2022-08-02 23:50:08 +0300

    str methods

--- backend/django/api_p/poe_snapsht/models.py
+++ backend/django/api_p/poe_snapsht/models.py
@@ -4,11 +4,17 @@ from django.db import models
 class Accounts(models.Model):
     account_name = models.CharField(max_length=200)
 
+    def __str__(self) -> str:
+        return self.account_name
+
 class Characters(models.Model):
     account = models.ForeignKey(Accounts, on_delete=models.CASCADE)
     character = models.CharField(max_length=200)
     league = models.CharField(max_length=200)
     tracked = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.character
 
 class SnapShots(models.Model):
     character_id = models.ForeignKey(Characters, on_delete=models.CASCADE)
@@ -16,3 +22,6 @@ class SnapShots(models.Model):
     items = models.JSONField()
     passives = models.JSONField()
     time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.character_id}:{self.time}'
