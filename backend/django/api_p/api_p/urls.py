1f37619bbfdfd26c4e306c74a2931a490e282912
fluxink <pavelvolk300@gmail.com>
2022-08-02 23:06:30 +0300

    views and urls

--- backend/django/api_p/api_p/urls.py
+++ backend/django/api_p/api_p/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from poe_snapsht import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/<account_name>', views.CharList.as_view()),
+    path('api/<account_name>/<character>', views.SnapShotsList.as_view())
 ]
--- backend/django/api_p/poe_snapsht/views.py
+++ backend/django/api_p/poe_snapsht/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import SnapShots, Characters, Accounts
+from .serializers import CharSerializer, SnapShotSerializer
 
-# Create your views here.
+
+class SnapShotsList(APIView):
+
+    def get(self, request, character, format=None, **kwargs):
+        char = Characters.objects.get(character=character)
+
+        snapshots = SnapShots.objects.filter(character_id=char)
+        serializer = SnapShotSerializer(snapshots, many=True)
+
+        return Response(serializer.data)
+
+
+class CharList(APIView):
+
+    def get(self, request, account_name, format=None):
+        account = Accounts.objects.get(account_name=account_name)
+
+        chars = Characters.objects.filter(account_id=account)
+        serializer = CharSerializer(chars, many=True)
+
+        return Response(serializer.data)
