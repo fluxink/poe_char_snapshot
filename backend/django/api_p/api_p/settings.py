ead31b21debd12e1cc821f6a7234ff89d4b64d33
fluxink <pavelvolk300@gmail.com>
2022-08-03 00:08:03 +0300

    ой

--- backend/django/api_p/api_p/settings.py
+++ backend/django/api_p/api_p/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+c!td$(s36j#p&%b&tsk-%$5h$7*_hy=0ge@fpab0yt_mxksdg'
+SECRET_KEY = os.environ.get('POEX_SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
